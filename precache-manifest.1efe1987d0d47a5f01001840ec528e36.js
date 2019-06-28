de0396c70ca58955b26bc36890a90223bc09ca98
Amit Nautiyal <amit.nautiyal@crssoftware.co.nz>
2019-06-29 09:38:26 +1200

    Updates

---
+++ precache-manifest.1efe1987d0d47a5f01001840ec528e36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3184459dd48151093b9d9038579d44c",
+    "url": "/products-page/index.html"
+  },
+  {
+    "revision": "10e47b8317926fe96d2e",
+    "url": "/products-page/static/css/main.94aa68f2.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/products-page/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "10e47b8317926fe96d2e",
+    "url": "/products-page/static/js/main.9db7d664.chunk.js"
+  },
+  {
+    "revision": "24e6a2456b391d5ca91b",
+    "url": "/products-page/static/js/runtime~main.f5a77835.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/products/precache-manifest.3c9559d26a02163891ab289c2fd6d37a.js"
+  "/products-page/precache-manifest.1efe1987d0d47a5f01001840ec528e36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/products/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/products-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
